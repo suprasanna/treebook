a1afd98b4d19e5a3d819e34b35dd584d1d170e07
Suprasanna Mishra <yknihc@gmail.com>
2013-03-24 19:12:51 -0400

    First git push for this app. Created a rails app and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '5b2403e5cb02dbd4fb2a9fc198d6e2912416a90707ae644ab598f3b8b1088bb2d90587f40a5961ad4fc8e7999921da52b87d949b29e584428448810d92d0ec0a'
